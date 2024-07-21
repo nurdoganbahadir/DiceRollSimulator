b085461306164c06bef0c56d36ed977fde4e7682
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-21 21:13:45 +0300

    update p

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const result = document.querySelector(".listRollHistory");
 const rollDice = document.querySelector(".rollDiceFace");
 
 let historyList = [];
+let i = 1;
 
 rollBtn.onclick = function () {
   const rollResult = Math.floor(Math.random() * 6) + 1;
@@ -12,26 +13,32 @@ rollBtn.onclick = function () {
       rollDice.innerHTML = "&#9856;";
       listRoll(1);
       historyList = [];
+      i += 1;
     } else if (sayi === 2) {
       rollDice.innerHTML = "&#9857;";
       listRoll(2);
       historyList = [];
+      i += 1;
     } else if (sayi === 3) {
       rollDice.innerHTML = "&#9858;";
       listRoll(3);
       historyList = [];
+      i += 1;
     } else if (sayi === 4) {
       rollDice.innerHTML = "&#9859;";
       listRoll(4);
       historyList = [];
+      i += 1;
     } else if (sayi === 5) {
       rollDice.innerHTML = "&#9860;";
       listRoll(5);
       historyList = [];
+      i += 1;
     } else {
       rollDice.innerHTML = "&#9861;";
       listRoll(6);
       historyList = [];
+      i += 1;
     }
   }
 };
@@ -40,16 +47,16 @@ function listRoll(e) {
   const listItem = document.createElement("p");
   result.appendChild(listItem);
   if (e == 1) {
-    listItem.innerHTML = "&#9856;";
+    listItem.innerHTML = `Roll ${i}: ${"&#9856;"}`;
   } else if (e == 2) {
-    listItem.innerHTML = "&#9857;";
+    listItem.innerHTML = `Roll ${i}: ${"&#9857;"}`;
   } else if (e == 3) {
-    listItem.innerHTML = "&#9858;";
+    listItem.innerHTML = `Roll ${i}: ${"&#9858;"}`;
   } else if (e == 4) {
-    listItem.innerHTML = "&#9859;";
+    listItem.innerHTML = `Roll ${i}: ${"&#9859;"}`;
   } else if (e == 5) {
-    listItem.innerHTML = "&#9860;";
+    listItem.innerHTML = `Roll ${i}: ${"&#9860;"}`;
   } else {
-    listItem.innerHTML = "&#9861;";
+    listItem.innerHTML = `Roll ${i}: ${"&#9861;"}`;
   }
 }
